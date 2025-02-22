51949ff8cd7be42a4cf04777d3416d50e4070e8f
Paolo Flores <pflores.fisi22@gmail.com>
2025-02-22 01:51:39 -0500

    deploy redis

--- reports-service/src/main.ts
+++ reports-service/src/main.ts
@@ -2,6 +2,7 @@ import { Elysia } from 'elysia'
 import { PORT } from '@/env';
 import { sql } from 'bun';
 import { minio } from './minio';
+import { redis } from './redis';
 
 new Elysia()
     .get('/', async () => {
@@ -9,6 +10,8 @@ new Elysia()
         console.log(rows[0].version);
         return rows[0].version;
     })
+    // TODO health check
+    // TODO queue integration
     .get('/minio', async () => {
         const hash = Math.random().toString(36).substring(7);
         const filename = `test_${hash}.txt`;
@@ -16,6 +19,14 @@ new Elysia()
         const f = minio.file(filename);
         await f.write(`🐢 working 🐢 - ${hash} `);
         return `🐢 ${filename} was created`;
+    })
+    .get('/redis/set/:key/:value', async ({ params }) => {
+        await redis.set(params.key, params.value);
+        return `🔑 ${params.key} set to ${params.value}`;
+    })
+    .get('/redis/get/:key', async ({ params }) => {
+        const value = await redis.get(params.key);
+        return `🔑 ${params.key} is ${value}`;
     })
     .ws('/echo', {
         message(ws, message) {
