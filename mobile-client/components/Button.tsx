92759565843d22e156b37dc32c55173e048981a5
Stevan <stevanpersonal@gmail.com>
2025-02-25 00:55:28 -0500

    feat(Button): add basic support for variants

--- mobile-client/components/Button.tsx
+++ mobile-client/components/Button.tsx
@@ -2,17 +2,31 @@ import { Pressable, Text, PressableProps, Button } from "react-native";
 
 export type ButtonProps = PressableProps & {
   label: string
-  danger?: boolean
+  variant?: 'primary' | 'secondary' | 'danger'
 }
 
-export default (props: ButtonProps) => {
+export default ({ label, variant = 'primary', onPress, ...props }:
+  ButtonProps
+) => {
   return (
     <Pressable
-      className={`rounded-xl h-16 items-center justify-center p-4 bg-inverse active:bg-inverse-mild`}
-      onPress={props.onPress}
+      className={`rounded-xl h-16 items-center justify-center p-4 ${
+        variant === 'primary'   ? 'bg-inverse active:bg-inverse-mild' :
+        variant === 'secondary' ? 'bg-foreground active:bg-foreground-mild' :
+        'bg-danger active:bg-red-600'
+      }`}
+      onPress={onPress}
       {...props}
     >
-      <Text className="color-inverse font-medium">{props.label}</Text>
+      <Text
+        className={`font-medium ${
+          variant === 'primary' ? 'text-inverse' :
+          variant === 'secondary' ? 'text-default' :
+          'text-white'
+        }`}
+      >
+        {label}
+      </Text>
     </Pressable>
   );
 }
