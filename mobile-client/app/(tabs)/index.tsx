d825a7d776304271d71bc126da8e2a1e2b3ed983
Paolo Flores <pflores.fisi22@gmail.com>
2025-02-25 14:59:18 -0500

    add map view

--- mobile-client/app/(tabs)/index.tsx
+++ mobile-client/app/(tabs)/index.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import Mapbox from '@rnmapbox/maps';
 
 export default function Index() {
   return (
-    <View>
-      <Text>Inicio</Text>
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <Mapbox.MapView style={styles.map} />
+      </View>
     </View>
   );
 }
+
+Mapbox.setAccessToken('MAPBOX_API_KEY');
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  map: {
+    flex: 1
+  }
+});
