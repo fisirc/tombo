8dedf1f58eba9579db3f9b042022c3a2bf49139d
Stevan <stevanpersonal@gmail.com>
2025-02-25 03:55:32 -0500

    feat: add TextArea component

---
+++ mobile-client/components/TextArea.tsx
@@ -0,0 +1,21 @@
+import { Text, TextInput, TextInputProps, View } from 'react-native'
+
+export type TextAreaProps = TextInputProps & {
+  label: string
+}
+
+export default (props: TextAreaProps) => {
+  return (
+    <View className='flex gap-2'>
+      <Text className='text-default'>
+        {props.label}
+      </Text>
+      <TextInput
+        multiline
+        textAlignVertical='top'
+        className='bg-foreground-mild h-32 text-default p-4 rounded-xl placeholder:text-muted'
+        {...props}
+      />
+    </View>
+  )
+}
