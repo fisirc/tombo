849af0825c6e9bc781282ee0eec8ac31b91eb374
Stevan <stevanpersonal@gmail.com>
2025-02-25 04:56:04 -0500

    feat(Select): add search support

--- mobile-client/components/Select.tsx
+++ mobile-client/components/Select.tsx
@@ -4,10 +4,11 @@ import { Pressable, Text, View } from "react-native"
 import SelectDropdown, { SelectDropdownProps } from "react-native-select-dropdown"
 
 export type SelectProps = Omit<SelectDropdownProps, 'renderItem' | 'renderButton'> & {
+  placeholder: string
   label: string
 }
 
-export type Item = {
+export type SelectItem = {
   label: string
   value: string | number
 }
@@ -21,6 +22,11 @@ export default (props: SelectProps) => {
     strokeWidth: 2
   }
 
+  const total = props.data.length
+
+  let dropdownHeight = 175 + (props.search ? 50 : 0)
+  if (total <= 3) dropdownHeight = 50 * total
+
   return (
     <View className='flex gap-2'>
       <Text className='text-default'>
@@ -28,12 +34,13 @@ export default (props: SelectProps) => {
       </Text>
       <SelectDropdown
         {...props}
-        onSelect={({ value }: Item) => props.onSelect(value, props.data.find(item => item.value === value))}
+        onSelect={({ value }: SelectItem) => props.onSelect(value, props.data.find(item => item.value === value))}
         dropdownStyle={{
           borderRadius: 8,
-          backgroundColor: theme['--color-bg-default']
+          backgroundColor: theme['--color-bg-default'],
+          height: dropdownHeight,
         }}
-        renderItem={(item: Item, isSelected) => (
+        renderItem={(item: SelectItem, isSelected) => (
           <View
             // className={`flex-1 flex justify-center p-4 ${isSelected ? 'bg-inverse' : 'bg-transparent'}`}
             className='flex-1 flex justify-center p-4 bg-transparent'
@@ -46,16 +53,24 @@ export default (props: SelectProps) => {
             </Text>
           </View>
         )}
-        renderButton={(selectedItem: Item, isOpened) => (
+        renderButton={(selectedItem: SelectItem, isOpened) => (
           <View className='rounded-xl h-16 flex flex-row justify-between items-center p-4 bg-foreground-mild'>
-            <Text className="text-default">
-              {selectedItem?.label || 'Seleccione un valor'}
+            <Text className={selectedItem ? 'text-default' : 'text-muted'}>
+              {selectedItem?.label || props.placeholder || 'Buscar...'}
             </Text>
             {isOpened
             ? <IconChevronUp {...chevronProps} />
             : <IconChevronDown {...chevronProps} />}
           </View>
         )}
+        searchInputStyle={{
+          backgroundColor: theme['--color-bg-default'],
+          paddingLeft: 11,
+          paddingRight: 11,
+        }}
+        searchPlaceHolderColor={theme['--color-text-muted']}
+        searchInputTxtColor={theme['--color-text-default']}
+        searchPlaceHolder={props.searchPlaceHolder || 'Buscar...'}
       />
     </View>
   )
