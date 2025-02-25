dfd24d8758f0aca13ab41fd54d556eef21c2b936
Stevan <stevanpersonal@gmail.com>
2025-02-25 01:09:57 -0500

    chore: change project structure

--- mobile-client/app/(tabs)/index.tsx
+++ mobile-client/app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View>
       <Text>Inicio</Text>
     </View>
   );
