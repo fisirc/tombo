9e908beb6e88dd2724a0769404c8dca14a8a80fd
Paolo Flores <pflores.fisi22@gmail.com>
2025-02-25 15:09:17 -0500

    show location on map

--- mobile-client/app/(tabs)/index.tsx
+++ mobile-client/app/(tabs)/index.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-import Mapbox from '@rnmapbox/maps';
+import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Mapbox, { Camera, UserLocation, UserTrackingMode } from '@rnmapbox/maps';
+
+const DISPLACEMENT = [0, 5, 10];
 
 export default function Index() {
+  useEffect(() => {
+    (async () => {
+      await Mapbox.requestAndroidLocationPermissions();
+      Mapbox.locationManager.start();
+    })();
+
+    return () => {
+      Mapbox.locationManager.stop();
+    };
+  }, []);
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} />
+        <Mapbox.MapView
+          style={styles.map}
+          compassEnabled={true}
+          logoEnabled={false}
+          attributionEnabled={false}
+          scaleBarEnabled={false}
+        >
+          <Camera
+            followZoomLevel={16}
+            followUserMode={UserTrackingMode.Follow}
+            followUserLocation
+          />
+
+          <UserLocation minDisplacement={DISPLACEMENT[0]} />
+        </Mapbox.MapView>
       </View>
     </View>
   );
 }
 
-Mapbox.setAccessToken('MAPBOX_API_KEY');
+Mapbox.setAccessToken('pk.eyJ1IjoiZG90eWVpc29uIiwiYSI6ImNsdGdsbWc0OTAxbXQyanAyaGFwY20xN2MifQ.zqaP-jWUgc1FYPfcHIjtVw');
 
 const styles = StyleSheet.create({
   page: {
